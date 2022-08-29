d0537f75df3857cc292d060d8461323e034eb52c
sikaha <987555458@qq.com>
2022-08-29 19:16:48 +0800

    t4

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 function name(params) {}
+console.log(12413);
+console.log(12413);
+console.log(12413);
+console.log(12413);
 function name(params) {}
-function name(params) {}
-function name(params) {}
-//this is sikara
